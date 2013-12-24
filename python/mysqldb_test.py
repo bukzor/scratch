2449f1ec2934c0c06ba060df8681c35333403382
Buck Golemon <workitharder@gmail.com>
2013-12-23 19:17:55 -0800

    py.test compat.

--- python/mysqldb_test.py
+++ python/mysqldb_test.py
@@ -1,13 +1,17 @@
 # encoding:UTF-8
 
-from MySQLdb import connect
+def main():
+    from MySQLdb import connect
 
-# TODO: resolve the ~
-conn = connect(user='root', unix_socket='~/tmp/mysql/mysql.sock', charset='utf8')
+    # TODO: resolve the ~
+    conn = connect(user='root', unix_socket='~/tmp/mysql/mysql.sock', charset='utf8')
 
-#conn.set_character_set('utf8')
+    #conn.set_character_set('utf8')
 
-cursor = conn.cursor()
-cursor.execute('select %s', u"Łódź")
-for row in cursor:
-	print row[0]
+    cursor = conn.cursor()
+    cursor.execute('select %s', u"Łódź")
+    for row in cursor:
+            print row[0]
+
+if __name__ == '__main__':
+    main()
--- python/signal_test.py
+++ python/signal_test.py
@@ -1,7 +1,10 @@
 from time import sleep
 
+def main():
+    try:
+        sleep(10000)
+    finally:
+        print "Caught!"
 
-try:
-	sleep(10000)
-finally:
-	print "Caught!"
+if __name__ == '__main__':
+    main()
