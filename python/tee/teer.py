4cd8e1f152524d270bc66d4d53aabdc57331bdd0
Buck Golemon <buck@yelp.com>
2014-10-24 14:34:42 -0700

    py34 support

--- python/tee/teer.py
+++ python/tee/teer.py
@@ -2,8 +2,13 @@
 Show a command's output in realtime and capture its outputs as strings,
 without deadlocking or temporary files.
 """
+from __future__ import print_function
+
 import os
 from subprocess import Popen
+
+PY3 = (str is not bytes)
+
 
 # posix standard file descriptors
 STDIN, STDOUT, STDERR = range(3)
@@ -22,6 +27,11 @@ class Pipe(object):
     """a convenience object, wrapping os.pipe()"""
     def __init__(self):
         self.read, self.write = os.pipe()
+        if PY3:
+            print('PY3')
+            # emulate old os.pipe
+            os.set_inheritable(self.read, True)
+            os.set_inheritable(self.write, True)
 
     def closed(self):
         """close both ends of the pipe. idempotent."""
@@ -87,13 +97,13 @@ def _communicate_with_select(read_set):
 
         for fd in readable:
             data = os.read(fd, 1024)
-            if data == "":
+            if data == b"":
                 os.close(fd)
                 read_set.remove(fd)
             result[fd].append(data)
 
     return tuple(
-        ''.join(result[fd])
+        b''.join(result[fd])
         for fd in orig_read_set
     )
 
@@ -113,6 +123,7 @@ def run(cmd):
         cmd,
         stdout=stdout_orig.write,
         stderr=stderr_orig.write,
+        close_fds=False,  # default changed to True in py3
     )
     stdout_orig.readonly()  # deadlock otherwise
     stderr_orig.readonly()  # deadlock otherwise
@@ -148,18 +159,19 @@ def demo():
     """Demonstrate that run() doesn't deadlock,
     even under worst-case conditions.
     """
-    cmd = ('python', 'outputter.py')
-    print 'CMD:', cmd
+    from sys import executable
+    cmd = (executable, 'outputter.py')
+    print('CMD:', cmd)
     stdout, stderr, combined = run(cmd)
 
-    print 'STDOUT:'
-    print stdout.count('\n')
+    print('STDOUT:')
+    print(stdout.count(b'\n'))
 
-    print 'STDERR:'
-    print stderr.count('\n')
+    print('STDERR:')
+    print(stderr.count(b'\n'))
 
-    print 'COMBINED:'
-    print combined.count('\n')
+    print('COMBINED:')
+    print(combined.count(b'\n'))
 
 
 def make_outputter():
@@ -178,6 +190,14 @@ TIME = 2
 WIDTH = 179
 ERROR_RATIO = .40
 
+PY3 = (str is not bytes)
+if PY3:
+    # emulate the old, less-buffered print
+    orig_print = print
+    def print(*args, **kwargs):
+        kwargs.setdefault('flush', True)
+        orig_print(*args, **kwargs)
+
 for i in range(LINES):
     if random() > ERROR_RATIO:
         char = '.'
@@ -190,6 +210,8 @@ for i in range(LINES):
         print(char, file=file, end='')
         sleep(TIME / LINES / WIDTH)
     print(file=file)
+print('EOF:stderr', file=stderr)
+print('EOF:stdout', file=stdout)
 ''')
 
 
